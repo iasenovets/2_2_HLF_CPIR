50fe32b89868fc863601010840e8a9dea94bebdb
artias13 <archee.busy@gmail.com>
2025-09-23 01:42:26 +0800

    notations update, packing constraints, exp matrix figure

--- code_branch_2/off_chain_code/off_chain_pir_client/cmd/client/main.go
+++ code_branch_2/off_chain_code/off_chain_pir_client/cmd/client/main.go
@@ -10,15 +10,18 @@ import (
 
 /********* main demo **********************************************/
 func main() {
-	//const dbSize = 100
-	const idx = 13
-	const logN = 13 // available values: 13..15
+	// --- Set parameters --- Please follow the Feasible Parameters table in the README.md
+	const logN = 13          // set the HE parameter LogN: 13, 14, or 15
+	const dbSize = 128       // set the total number of records in the DB: 100, 256, or 512
+	const maxJSONlength = 64 // set the max JSON length: 64, 128, 224, 256, 384, or 512
+	const idx = 13           // set the index of the record to be retrieved: 0..dbSize-1
+	fmt.Printf("Demo with LogN=%d, dbSize=%d, maxJSONlength=%d, retrieving record idx=%d\n", logN, dbSize, maxJSONlength, idx)
 
 	// HE keys
 	params, sk, pk, _ := cpir.GenKeys(logN)
 
-	// --- Init --- numRecords | maxJSONlength | channel_name
-	utils.Call("InitLedger", "256", "64", fmt.Sprintf("%d", logN))
+	// --- Init --- dbSize | maxJSONlength | logN
+	utils.Call("InitLedger", fmt.Sprintf("%d", dbSize), fmt.Sprintf("%d", maxJSONlength), fmt.Sprintf("%d", logN))
 
 	slotsStr, _ := utils.Call("GetSlotsPerRecord")
 	slotsPerRec, _ := strconv.Atoi(slotsStr)
@@ -33,18 +36,18 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Total CTI records =", totalStr)
-	dbSize, err := strconv.Atoi(totalStr)
+	serverDbSize, err := strconv.Atoi(totalStr)
 	if err != nil {
 		panic(fmt.Errorf("PublicQueryALL returned non-number %q: %w", totalStr, err))
 	}
 
-	fmt.Printf("CTI record count = %d\n", dbSize)
+	fmt.Printf("CTI record count = %d\n", serverDbSize)
 
 	// --- PIR
-	encQueryB64, len_ct_bytes, _ := cpir.EncryptQueryBase64(params, pk, idx, dbSize, slotsPerRec)
+	encQueryB64, len_ct_bytes, _ := cpir.EncryptQueryBase64(params, pk, idx, serverDbSize, slotsPerRec)
 	fmt.Printf("len_ct_bytes=%d\n", len_ct_bytes)
 
 	encResB64, _ := utils.Call("PIRQuery", encQueryB64)
-	dec, _ := cpir.DecryptResult(params, sk, encResB64, idx, dbSize, slotsPerRec)
+	dec, _ := cpir.DecryptResult(params, sk, encResB64, idx, serverDbSize, slotsPerRec)
 	fmt.Println("PIR result =", dec.JSONString)
 }
